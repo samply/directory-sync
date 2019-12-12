3a25c63f5557eefad60746db2414c08f3b94cc6a
Martin Breu <breu_m@ukw.de>
2019-12-12 15:23:25 +0100

    Add Directory connection draft

--- src/main/java/de/samply/directory_sync/Main.java
+++ src/main/java/de/samply/directory_sync/Main.java
@@ -4,12 +4,79 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.MeasureReport;
 import org.hl7.fhir.r4.model.Parameters;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
+        //System.out.println(getBlaze());
+        System.out.println(getDirectory());
+    }
+
+
+    static class LoginResponse {
+        String username, token;
+
+        LoginResponse() {
+        }
+    }
+
+
+    static String getDirectory() throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8081/api/v1/login");
+
+        String json = "{\n" +
+                "  \"username\": \"admin\",\n" +
+                "  \"password\": \"admin\"\n" +
+                "}";
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+
+        CloseableHttpResponse tokenResponse = client.execute(httpPost);
+        String body = EntityUtils.toString(tokenResponse.getEntity());
+        Gson gson = new Gson();
+        LoginResponse loginResponse = gson.fromJson(body, LoginResponse.class);
+        String token = loginResponse.token;
+
+        HttpGet httpGet = new HttpGet("http://localhost:8081/api/v2/sys_md_EntityType");
+        httpGet.setHeader("x-molgenis-token", token);
+        httpGet.setHeader("Accept", "application/json");
+        httpGet.setHeader("Content-type", "application/json");
+
+        CloseableHttpResponse response = client.execute(httpGet);
+        String result = EntityUtils.toString(response.getEntity());
+        client.close();
+
+        return result;
+    }
+
+
+    static String getBlaze() {
+
         // Create a client to talk to the HeathIntersections server
         FhirContext ctx = FhirContext.forR4();
         IGenericClient client = ctx.newRestfulGenericClient("https://blaze.life.uni-leipzig.de/fhir");
@@ -21,7 +88,7 @@ public class Main {
         inParams.addParameter().setName("periodEnd").setValue(new DateType("2100"));
         //TODO add Measure canonicalUrl as Parameter
 
-    // Invoke $everything on "Patient/1"
+        // Invoke $everything on "Patient/1"
         Parameters outParams = client
                 .operation()
                 .onInstance(new IdDt("Measure", "5ddac8ad-371c-4e25-924d-46281964b348"))
@@ -39,7 +106,7 @@ public class Main {
          */
         MeasureReport response = (MeasureReport) outParams.getParameter().get(0).getResource();
 
-// Print the response bundle
-        System.out.println(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(response));
+        // Print the response bundle
+        return (ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(response));
     }
 }
